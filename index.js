355f9200e0d368812caeb799dc50bd640e6565d4
Soufiane RAHJ <soufiane.rahj@instant-system.com>
2022-08-13 09:07:39 +0200

    Added messages when a user connects or disconnects

--- index.js
+++ index.js
@@ -9,10 +9,18 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+const broadcastChatMessage = (mes) => {
+  io.emit('chat message', mes);
+}
+
 io.on('connection', (socket) => {
+    broadcastChatMessage('A user disconnected');
+
     socket.on('chat message', (msg) => {
-        io.emit('chat message', msg);
+        broadcastChatMessage(msg);
       });
+      
+    socket.on('disconnect', broadcastChatMessage('A user disconnected'));
   });
 
 server.listen(3000, () => {
